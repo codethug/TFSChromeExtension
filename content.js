00f401d37c4ba187f51f153907fe71a77faa2d75
Tim Larson <tlarbo@gmail.com>
2013-09-11 12:34:50 -0400

    DOM data updated, loaded with OOTB code
    The server passes down some JSON in a script tag inside the
    div#taskboard.  This is the initial data that is used to populate the
    task board.  This checkin updates that JSON before the page uses it so
    that we can add the Blocked field and to each work item in that JSON.
    
    This causes our custom data to be loaded into the standard data model
    for the page.
    
    Additionally, the custom renderer that we have written that helps to
    draw the colors (_updateTileColors) now uses the Blocked field that
    exists in the standard data model.

--- content.js
+++ content.js
@@ -1,25 +1,101 @@
 function injectedCode () {
+
+	var settingsTag = $("#tfsExtensionSettings").eq(0);
+	var extensionSettings = JSON.parse(settingsTag.html());
 	
-	require(['Agile/Scripts/TFS.Agile.TaskBoard.View', "Presentation/Scripts/TFS/TFS.OM",
+	if (extensionSettings.blockedAsRed === "true")
+	{
+		var modelScript = $("#taskboard script").eq(0);
+		var model = JSON.parse(modelScript.html());
+		// Add extra data to model and save it back to the DOM before it gets loaded
+
+		var workItems = Object.keys(model.payload.data);
+		var query = generateQuery(workItems, ['Blocked']);
+
+		getData(query, function(result){
+			updateModel(model, result);
+			modelScript.html(JSON.stringify(model));
+		}, true);
+	}
+	
+	function generateQuery(workItems, fields)
+	{
+		// Add System.Id to the front of the list of fields
+		fields.unshift("System.Id");
+
+		var token = document.getElementsByName("__RequestVerificationToken")[0].value;
+				
+		var query = 
+			'workItemIds=' + workItems.join() + // '50,45,32'
+			'&fields=' + encodeURIComponent(fields.join()) +
+			'&__RequestVerificationToken=' + token;
+
+		return query;
+	}
+			
+	function getData(query, callback, synchronous)
+	{
+		if (typeof synchronous === "undefined" || synchronous == null) { synchronous = false; }
+
+		var xmlhttp=new XMLHttpRequest();
+		xmlhttp.open("POST", window.location.origin + '/DefaultCollection/_api/_wit/pageWorkItems?__v=4', !synchronous);
+		xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		xmlhttp.onload = function () {
+			// do something to response
+			var jsonResult = JSON.parse(this.responseText);
+			return callback(jsonResult);
+		};
+		
+		xmlhttp.send(query);
+	}
+
+	function updateModel(model, newData)
+	{
+		// Remove System.Id from list of columns
+		newData.columns.shift();
+		// Add the columns to our array of columns
+		model.payload.columns = model.payload.columns.concat(newData.columns);
+			
+		// Add the data for each item
+		newData.rows.forEach(function(row, index){
+			// get the id and remove it from the row			
+			var id = row.shift();
+			// add the rest of the data to our model
+			model.payload.data[id] = model.payload.data[id].concat(row); 
+		});
+	}
+
+	require([
+		'Agile/Scripts/TFS.Agile.TaskBoard', 
+		'Agile/Scripts/TFS.Agile.TaskBoard.View', 
+		"Presentation/Scripts/TFS/TFS.OM",
 		"Presentation/Scripts/TFS/Generated/TFS.WorkItemTracking.Constants",
-		"Agile/Scripts/TFS.Agile.Utils"], function(taskBoard, t, workItemConstants, agileUtils) {
+		"Agile/Scripts/TFS.Agile.Utils"], function(taskBoard, taskBoardView, t, workItemConstants, agileUtils) {
 
 		var coreFieldNames = agileUtils.DatabaseCoreFieldRefName;
 
-		debugger;
-		
-		taskBoard.TaskBoardView.prototype._updateTileColors = function(outerTileElement, taskId, tileMatchesFilter) {
+		taskBoard.TaskBoard.prototype.getModel = function()
+		{
+			return this._model;
+		}
+
+		var foo = taskBoard.TaskBoard.prototype.getModel();
+
+		taskBoardView.TaskBoardView.prototype._updateTileColors = function(outerTileElement, taskId, tileMatchesFilter) {
 		
 			var colorsProvider = t.ColorsProvider.getDefault();
 			if (colorsProvider.isWorkItemColorsDefined()) {
 				var workItemType = this._getFormattedFieldValue(taskId, workItemConstants.CoreFieldRefNames.WorkItemType);
-				var innerTileElement = $(".tbTileContent", outerTileElement);
 
-	//			if innerTileElement.data("IsBlocked" || true)
-				if (true)
+				var isBlocked = (extensionSettings.blockedAsRed === "true") && 
+								(this._getFormattedFieldValue(taskId, "Microsoft.VSTS.CMMI.Blocked") === "Yes");
+				
+				var innerTileElement = $(".tbTileContent", outerTileElement);
+		
+				if (isBlocked)
 				{
 					// Custom
-					innerTileElement.css("border-left-color", "blue")
+					innerTileElement.css("border-left-color", "Red")
 				}
 				else
 				{
@@ -33,106 +109,24 @@ function injectedCode () {
 		}
 
 	});
-
+	
 }
-
-var script = document.createElement('script');
-script.setAttribute("async","async");
-script.appendChild(document.createTextNode('('+ injectedCode +')();'));
-(document.body || document.head || document.documentElement).appendChild(script);
-
-
 
 chrome.runtime.sendMessage({method: "getLocalStorage"}, function(localStorage) {
 
 	console.group("TFS Extensions");
 	console.log("localStorage retrieved: %O", localStorage);
+	console.groupEnd();
 
-	// Show blocked items as red
-	if (typeof localStorage.blockedAsRed !== "undefined" && localStorage.blockedAsRed !== "false")
-	{	
-		var allChildWorkItems = getAllChildWorkItemsOnBoard();
-		var token = document.getElementsByName("__RequestVerificationToken")[0].value;
-		
-		console.log("Child Work Items: %O", allChildWorkItems);
-		
-		var data = 
-			'workItemIds=' + allChildWorkItems.join() + // '50,45,32'
-			'&fields=System.Id%2CSystem.Title%2CBlocked' +
-			'&__RequestVerificationToken=' + token;
+	var settingsTag = document.createElement('script');
+	settingsTag.setAttribute("type", "application/json");
+	settingsTag.id = "tfsExtensionSettings";
+	settingsTag.appendChild(document.createTextNode(JSON.stringify(localStorage)));
+	(document.body || document.head || document.documentElement).appendChild(settingsTag);
 
-		console.log("Query: %O", data);
-			
-		var xmlhttp=new XMLHttpRequest();
-		xmlhttp.open("POST", window.location.origin + '/DefaultCollection/_api/_wit/pageWorkItems?__v=4');
-		xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		xmlhttp.onload = function () {
-			// do something to response
-			var jsonResult = JSON.parse(this.responseText);
-			var blockedItems = determineBlockedItems(jsonResult);
-			
-			waitForElementToExist('tile-' + blockedItems[0], function() {
-				setBlockedItemsAsRed(blockedItems);
-			});
-		};
-		
-		xmlhttp.send(data);
-	}
-	else
-	{
-		console.log("blockedAsRed disabled");
-		console.groupEnd();
-	}
-});
-
-// returns: ["15", "30", "55"]
-function getAllChildWorkItemsOnBoard()
-{
-	var dataFromElement = JSON.parse(document.getElementsByClassName('aggregated-capacity-data')[0].textContent);
-	var idsAsObjectProperties = dataFromElement.previousValueData["Microsoft.VSTS.Scheduling.RemainingWork"];
-	var keysAsStringArray = Object.keys(idsAsObjectProperties);
-	return keysAsStringArray;
-}
-
-function determineBlockedItems(data)
-{
-	var blockedColumnNumber = data.columns.indexOf("Microsoft.VSTS.CMMI.Blocked");
-	var blockedItems = data.rows.filter(function(r) { return r[blockedColumnNumber] === "Yes" });
-
-	var idColumnNumber = data.columns.indexOf("System.Id");
-	var blockedIds = blockedItems.map(function(item) { return item[idColumnNumber]; });
+	var script = document.createElement('script');
+	script.setAttribute("async","async");
+	script.appendChild(document.createTextNode('('+ injectedCode +')();'));
+	(document.body || document.head || document.documentElement).appendChild(script);
 	
-	return blockedIds;
-}
-
-var maxAttempts = 3;
-var attemptsSoFar = 0;
-
-function waitForElementToExist(id, callback)
-{
-	if (attemptsSoFar === maxAttempts)
-	{
-		console.log("After " + maxAttempts + " attempts, we still can't find the appropriate DOM elements, so we're giving up");
-		console.groupEnd();
-		return;
-	}
-	attemptsSoFar++;
-
-	if (document.getElementById(id) == null)
-	{
-		console.error("element not found: " + id + ", will try again");
-		setTimeout(function() {
-			waitForElementToExist(id, callback);
-		}, 500);
-	}
-	callback();
-}
-
-function setBlockedItemsAsRed(itemArray)
-{
-	itemArray.forEach(function(id) {
-		var outerDiv = document.getElementById('tile-' + id);
-		var innerDiv = outerDiv.getElementsByClassName('tbTileContent')[0];		
-		innerDiv.style["border-left-color"]="red";
-	});
-}
+});
